4b7bab50e1b28fc9459a0134882d12b385c29b07
TheRedColossus <clem.saccoccio@protonmail.com>
2017-08-19 16:22:37 +0200

    portage to 1.9.4

--- src/main/java/com/trcgames/dbSynchronizer/database/ServerDatabase.java
+++ src/main/java/com/trcgames/dbSynchronizer/database/ServerDatabase.java
@@ -7,8 +7,8 @@ import com.trcgames.dbSynchronizer.packets.PacketServerToClient;
 import com.trcgames.dbSynchronizer.packets.PacketServerToClient.StCPacketType;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.WorldSavedData;
 import net.minecraft.world.storage.MapStorage;
-import net.minecraft.world.storage.WorldSavedData;
 import net.minecraftforge.common.DimensionManager;
 
 public class ServerDatabase extends WorldSavedData implements Database{
