20d0260340610f34629649dc3117a2b1f678625c
TheRedColossus <clem.saccoccio@protonmail.com>
2017-08-23 15:35:01 +0200

    release 1.1.0.2 (bug fix)

--- src/main/java/com/trcgames/dbSynchronizer/DBSynchronizer.java
+++ src/main/java/com/trcgames/dbSynchronizer/DBSynchronizer.java
@@ -23,7 +23,7 @@ public class DBSynchronizer{
 	public static DBSynchronizer instance;
 	
 	public static final String MOD_ID = "dbsynchronizer";
-	public static final String VERSION = "1.1.0.1";
+	public static final String VERSION = "1.1.0.2";
 	public static SimpleNetworkWrapper network;
 	public static boolean worldLoaded;
 	
